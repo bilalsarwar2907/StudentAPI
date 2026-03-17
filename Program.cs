65673e2f2cc168f0f2efc704764bd89c5e6128c6
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-18 00:28:46 +0100

    Add CORS support and enhance Swagger

--- Program.cs
+++ Program.cs
@@ -8,9 +8,39 @@ builder.Services.AddControllers();
 //DependencyInjection
 builder.Services.AddSingleton<IStudentRepositoryList, StudentRepositoryList>();
 
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//First Swagget
+
+//builder.Services.AddSwaggerGen();
+
+//Swagger Documentaion Test
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Student API",
+        Version = "v1",
+        Description = "API for managing students"
+    });
+});
+
+
+
 
 var app = builder.Build();
 //
@@ -22,5 +52,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseCors("AllowAll");
 app.MapControllers();
 app.Run();
