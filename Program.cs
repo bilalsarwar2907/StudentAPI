1b378955b04a69b2e4d0a01a6c460adb218a28ca
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-19 04:34:27 +0100

    Error_handling

--- Program.cs
+++ Program.cs
@@ -99,11 +99,15 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 //
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
